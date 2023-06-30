8383d26928ca057b709b287145c05634d07b8e82
EimantasBlazevicius <eima.blaz@gmail.com>
2023-06-30 10:57:12 +0300

    2023 U2

--- Uzduotys2023/U2_Slaptazodiai/u2.py
+++ Uzduotys2023/U2_Slaptazodiai/u2.py
@@ -1,4 +1,9 @@
 def kodu_panasumo_reiksme(kodas1, kodas2):
+    """
+    :param kodas1:
+    :param kodas2:
+    :return: Atemus reiksmes ir paemus ju absoliuciu reiksmiu suma gaunam panasumo indeksa
+    """
     ilgis = kodas1['ilgis'] - kodas2['ilgis']
     didziosios = kodas1['didziosios'] - kodas2['didziosios']
     mazosios = kodas1['mazosios'] - kodas2['mazosios']
@@ -9,6 +14,13 @@ def kodu_panasumo_reiksme(kodas1, kodas2):
 
 
 def maziausio_radimas(kodas, listas):
+    """
+    :param kodas:
+    :param listas:
+    :return: Naudodami kodu_panasumo_reiksme() funkcija
+    randam maziausia panasumo indeksa duotam kodui, duodam liste
+    Su idėja kad pagal nutylėjimą nepanasumas neperlips 99
+    """
     maziausias = 99
     for paruostas_kodas in listas:
         if kodu_panasumo_reiksme(kodas, paruostas_kodas) < maziausias:
@@ -17,12 +29,17 @@ def maziausio_radimas(kodas, listas):
     return maziausias
 
 
+# Programos pradzia
+# nuskaitom U2.txt i duomenys kontekstą
 with open('U2.txt') as duomenys:
-    slaptazodziu_kiekiai = duomenys.readline().split(' ')
-    vartotojo_pass_kiekis, paruostu_pass_kiekis = slaptazodziu_kiekiai
+    # nusiskaitom pirmą eilutę ir ją padalinam į du kintamuosius
+    vartotojo_pass_kiekis, paruostu_pass_kiekis = duomenys.readline().split(' ')
+    # inicijuoju tuščius listus į kuriuos paskui pridėsiu reikšmių
     vartotojo_passwords = []
     paruosti_passwords = []
 
+    # Du daug maž vienodi ciklai kad nuskaityt po žinomą kiekį eilučių iš failo
+    # ir jas sudėliojus į dictionary pridėt į atitinkamus sąrašus
     for _ in range(int(vartotojo_pass_kiekis)):
         kodas = duomenys.readline().strip().split(' ')
         vartotojo_passwords.append({'kodas': kodas[0], 'ilgis': int(kodas[1]), 'didziosios': int(kodas[2]),
@@ -33,22 +50,33 @@ with open('U2.txt') as duomenys:
         paruosti_passwords.append({'kodas': kodas[0], 'ilgis': int(kodas[1]), 'didziosios': int(kodas[2]),
                                'mazosios': int(kodas[3]), 'skaiciai': int(kodas[4]), 'spec': int(kodas[5]), 'lygis': kodas[6]})
 
+    # inicijuojam tuščia rezultatu sąrašą
     rezultatai = []
+    # kiekvienam kodui
     for vart_kodas in vartotojo_passwords:
+        # prisikiriam pavadinimą ir tuščią panašių kodų listą
         vartotojas = {'kodas': vart_kodas['kodas'], 'panasus': []}
+        # pagal nutylėjimą saugumo lygio nėra
         lygis = ''
+        # susirandu mažiausią šio kodo panašumą su listu
         maziausias_panasumas = maziausio_radimas(vart_kodas, paruosti_passwords)
+        # iteruoju per kodus kurie atitinka mano mažiausio panasumo reikalavimą ir prisiskiriu reiksmes
         for paruostas_kodas in paruosti_passwords:
             if kodu_panasumo_reiksme(vart_kodas, paruostas_kodas) <= maziausias_panasumas:
                 lygis = paruostas_kodas['lygis']
                 vartotojas['panasus'].append(paruostas_kodas['kodas'])
+        # pridedu reikamas reiksmes į dictionary
         vartotojas['maziausias_panasumas'] = maziausias_panasumas
         vartotojas['lygis'] = lygis
+        # prided dictionary į rezultatų sąrašą
         rezultatai.append(vartotojas)
 
+    # Rašom į failą
     with open('U2rez.txt', 'w') as output:
         for irasas in rezultatai:
             output.write(f"{irasas['kodas']} {irasas['maziausias_panasumas']} {irasas['lygis']}\n")
+            # išrykiuoju panašius kodus pagal ilgy nuo ilgiausio iki trumpiausio ir nieko nedarau kai kodai vienodo ilgio,
+            # [agal nutylėjimą sort funkcija nieko nekeis jeigu pagal kriterijų, negali
             irasas['panasus'].sort(key=len, reverse=True)
             for kodas in irasas['panasus']:
                 output.write(f"{kodas}\n")

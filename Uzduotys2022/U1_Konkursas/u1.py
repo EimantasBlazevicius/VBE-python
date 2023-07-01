196a949474ea7b3e034baa2ad3dcae09702e2e62
EimantasBlazevicius <eima.blaz@gmail.com>
2023-07-01 09:04:43 +0300

    2022 U1

--- Uzduotys2022/U1_Konkursas/u1.py
+++ Uzduotys2022/U1_Konkursas/u1.py
@@ -18,30 +18,39 @@ def dalyvio_rezultatai(dalyvis):
             'teisingi': teisingi_sprendimai, 'laikas': sprendimo_laikas}
 
 
-
+# Programos pradzia
+# Nuskaitom failą
 with open('U1.txt') as failas:
+    # Prisiskiriam reikšmes
     uzduociu_kiekis = int(failas.readline())
     uzduociu_laikai = failas.readline().strip().split(' ')
     uzduociu_taskai = failas.readline().strip().split(' ')
+    # nuskaitom dalyvius
     dalyviai = []
-
     for zmogus in failas.readlines():
         dalyvis = zmogus.strip().split(' ')
+        # issprendziam tą nesamonę kur gali būt du vardai
         if not dalyvis[1].isnumeric():
             dalyvis[0] += f" {dalyvis[1]}"
             dalyvis.pop(1)
+        # supildom dalyvius mum patogiu formatu
         dalyviai.append({'vardas': dalyvis[0], 'laikai': [laikas for laikas in dalyvis[1:]]})
 
+    # pasiruosiam kintamuosius rezultatam
     daugiausia_tasku = 0
     final_dalyviai = []
+    # performatuojam dalyvius i nauja listą
     for dalyvis in dalyviai:
         final_dalyviai.append(dalyvio_rezultatai(dalyvis))
+        # randam daugiausia taškų rezultata
         if daugiausia_tasku < dalyvio_rezultatai(dalyvis)['taskai']:
             daugiausia_tasku = dalyvio_rezultatai(dalyvis)['taskai']
 
 
+    # rašom i failą
     with open('U1rez.txt', 'w') as output:
         output.write(f"{daugiausia_tasku}\n")
+        # surikiuojam rezultatu faila pagal laiką nuo daugiausia uztrukusio iki trumpiausia
         for dalyvis in sorted(final_dalyviai, key=lambda x: x['laikas'], reverse=True):
             if dalyvis['taskai'] == daugiausia_tasku:
                 output.write(f"{dalyvis['vardas']} {dalyvis['teisingi']} {dalyvis['laikas']}\n")
